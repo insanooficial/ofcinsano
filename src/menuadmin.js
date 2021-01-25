b167a0f5bb33fa73de62d8ae2def5cc411d45c2a
vinizeus <viniciusonezio35@gmail.com>
2021-01-25 19:25:55 -0400

    Frost commit

---
+++ src/menuadmin.js
@@ -0,0 +1,33 @@
+const menuadmin = (prefix, pushname) => {
+ return `Olá esse é o menu dos admin
+ _obs o bot precisa ser admin para exutar esses comandos_
+ 
+ ◪ *COMANDO DOS ADMINS*
+ │
+ ├─ ❏ ${prefix}opengc
+ ├─ ❏ ${prefix}closegc
+ ├─ ❏ ${prefix}promote
+ ├─ ❏ ${prefix}demote
+ ├─ ❏ ${prefix}tagall
+ ├─ ❏ ${prefix}tagall2
+ ├─ ❏ ${prefix}tagall3
+ ├─ ❏ ${prefix}tagall4
+ ├─ ❏ ${prefix}tagall5
+ ├─ ❏ ${prefix}add
+ ├─ ❏ ${prefix}kick
+ ├─ ❏ ${prefix}listadmins
+ ├─ ❏ ${prefix}linkgroup
+ ├─ ❏ ${prefix}leave
+ ├─ ❏ ${prefix}welcome
+ ├─ ❏ ${prefix}nsfw
+ ├─ ❏ ${prefix}leveling
+ ├─ ❏ ${prefix}level
+ ├─ ❏ ${prefix}delete
+ ├─ ❏ ${prefix}simih
+ └─ ❏ ${prefix}ownergroup
+ `
+
+
+}
+
+exports.menuadmin = menuadmin
---
+++ src/zeusmenu.js
@@ -0,0 +1,12 @@
+const zeusmenu = (prefix, pushname) => {
+    return `◪ *Comandos do Zeus*
+    │
+    ├─ ❏ ${prefix}setprefix
+    ├─ ❏ ${prefix}block
+    ├─ ❏ ${prefix}bc
+    ├─ ❏ ${prefix}bcgc
+    └─ ❏ ${prefix}clearall`
+
+}
+
+exports.zeusmenu = zeusmenu
